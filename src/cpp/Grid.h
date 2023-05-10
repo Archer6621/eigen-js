7aed5edb58c697430a59c6a34313cf6c8003531b
Shaad Alaka <shaad1@live.nl>
2023-05-10 14:56:38 +0200

    Add print function to grid

--- src/cpp/Grid.h
+++ src/cpp/Grid.h
@@ -15,30 +15,44 @@ using namespace std;
 class Grid
 {
     //   using Mat = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
-    private:
-        Tensor<bool, 3> choices;
-        std::map<char,  Tensor<bool, 2> > adjacencies;
-        Tensor<bool, 2> childMask;
-        Tensor<bool, 1> leafMask;
+private:
+    Tensor<bool, 3> choices;
+    std::map<char, Tensor<bool, 2>> adjacencies;
+    Tensor<bool, 2> childMask;
+    Tensor<bool, 1> leafMask;
+    int width;
+    int height;
+    int tileChoices;
 
-    public:
-      Grid(int w, int h, int tileChoices, DenseMatrix<bool>& adjN, DenseMatrix<bool>& adjE, DenseMatrix<bool>& adjS,DenseMatrix<bool>& adjW, DenseMatrix<bool>& CM, DenseMatrix<bool>& LM)
-      {
-          choices = Tensor<bool, 3>(w, h, tileChoices).setConstant(true);
-          adjacencies['N'] = TensorCast(adjN.data, tileChoices, tileChoices);
-          adjacencies['E'] = TensorCast(adjE.data, tileChoices, tileChoices);
-          adjacencies['S'] = TensorCast(adjS.data, tileChoices, tileChoices);
-          adjacencies['W'] = TensorCast(adjW.data, tileChoices, tileChoices);
-          childMask = TensorCast(CM.data, tileChoices, tileChoices);
-          leafMask = TensorCast(LM.data, tileChoices);
-      }
+public:
+    Grid(int w, int h, int c, DenseMatrix<bool> &adjN, DenseMatrix<bool> &adjE, DenseMatrix<bool> &adjS, DenseMatrix<bool> &adjW, DenseMatrix<bool> &CM, DenseMatrix<bool> &LM)
+    {
+        choices = Tensor<bool, 3>(w, h, c).setConstant(true);
+        adjacencies['N'] = TensorCast(adjN.data, c, c);
+        adjacencies['E'] = TensorCast(adjE.data, c, c);
+        adjacencies['S'] = TensorCast(adjS.data, c, c);
+        adjacencies['W'] = TensorCast(adjW.data, c, c);
+        childMask = TensorCast(CM.data, c, c);
+        leafMask = TensorCast(LM.data, c);
+        width = w;
+        height = h;
+        tileChoices = c;
+    }
 
-
-
-
+    void print() const
+    {
+        for (int i = 0; i < this->width; i++)
+        {
+            printf(i == 0 ? "[[" : " [");
+            for (int j = 0; j < this->height; j++)
+            {
+                printf("%d", this->adjacencies.at('N')(i,j));
+                printf(j < this->height - 1 ? ", " : "");
+            }
+            printf((i < this->width - 1 ? "]" : "]]"));
+            printf("\n");
+        }
+    }
 };
-
-
-
 
 #endif // GRID_H
